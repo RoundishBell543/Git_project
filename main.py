216ffd70c3d0b54c4029b72902130eb4e3c8ef7e
Alex <lexfed0909@gmail.com>
2024-11-30 19:47:06 +0300

    Almast circle

--- main.py
+++ main.py
@@ -1,8 +1,78 @@
-def main():
-    print('Hello!')
-    print("kfk")
+import sys
+import io
+from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import  QApplication, QMainWindow
 
+template ="""<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>800</width>
+    <height>600</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <widget class="QPushButton" name="pushButton">
+    <property name="geometry">
+     <rect>
+      <x>140</x>
+      <y>370</y>
+      <width>191</width>
+      <height>101</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>Create yellow  circle</string>
+    </property>
+   </widget>
+  </widget>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
+"""
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        f = io.StringIO(template)
+        uic.loadUi(f, self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 0, 0))
+        qp.drawRect(30, 30, 120, 30)
+        qp.setBrush(QColor(0, 255, 0))
+        qp.drawRect(30, 60, 120, 30)
+        qp.setBrush(QColor(0, 0, 255))
+        qp.drawRect(30, 90, 120, 30)
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
